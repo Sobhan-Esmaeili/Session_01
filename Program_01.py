4ab4231ea5255593d2dec9f76d4f6b2977392e04
dr.sobhan.esmaeili <dr.sobhan.esmaeili@hotmail.com>
2023-10-13 00:03:32 +0330

    Step 01

--- Program_01.py
+++ Program_01.py
@@ -2,4 +2,32 @@
 Dr. Sobhan Esmaeili | Postdoctoral Researcher in Computer and Telecommunication Networks
 Statistical Pattern Recognition | Session 01
 """
+# Checking a Version Number
+# python --version
+# -------------------------
+
+# Clearing the Terminal Output
+# cls
+# -------------------------
+
+# Using the Python Shell
+# copyright
+# credits
+# -------------------------
+
+# Writing Your First Line of Python
+# print("Hello World")
+# -------------------------
+
+# Exiting the Python Shell
+# exit()
+# -------------------------
+
+# Line Numbers Introduced
+# 27| ←
+# -------------------------
+
+
+
+
 
